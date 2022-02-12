1645d75c7644ff24ff7aeca0104278e0ea170e15
Loyalfae <wafa.faquir@student.hamk.fi>
2022-02-12 17:42:44 +0200

    commit

---
+++ Login/Footer.php
@@ -0,0 +1,19 @@
+</div>
+<footer>
+    <div class="address">
+        <p><b>Hämeen ammattikorkeakoulu HAMK </b></p>
+        <p class="hamk"><b>PL 230 13101 Hämeenlinna</b></p>
+    </div>
+
+    <div class="social-media">
+        <ul>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-youtube"></i>
+        </ul>
+    </div>
+</footer>
+</body>
+</html>
