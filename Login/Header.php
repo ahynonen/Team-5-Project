f30b14874419142ef8132dc77adc2ea24d217f8a
Loyalfae <wafa.faquir@student.hamk.fi>
2022-02-14 21:21:23 +0200

    commit

--- Login/Header.php
+++ Login/Header.php
@@ -9,7 +9,7 @@
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
     />
     <title>Project</title>
-    <link rel="stylesheet" href="Css/Login.css"/>
+     <link rel="stylesheet" href="Css/Login.css"/>
     <link rel="stylesheet" href="Css/Css.css"/>
 </head>
 <body>
--- Login/login.php
+++ Login/login.php
@@ -25,6 +25,7 @@ include 'Header.php';
 		
 		<button>Login</button>
 	</form>
+	
 </div>
 
 
