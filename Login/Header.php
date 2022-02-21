0a4e3f9d72c7634fc4f152c5791c753895d17956
Loyalfae <wafa.faquir@student.hamk.fi>
2022-02-21 22:08:57 +0200

    commit

--- Login/Header.php
+++ Login/Header.php
@@ -4,7 +4,7 @@
         <a href="index.html">Home</a>
         <a href="portfolio.html">Portfolio</a>
         <a href="contact.html">Contact</a>
-        <a href="signup.php">Sign up</a>
+        <a href="/Users/loyalfae/Desktop/WafaWeb/src/Team-5-Project/signup/signup.php">Sign up</a>
         <a href="login.php">Login</a>
     </nav>
 </header>
