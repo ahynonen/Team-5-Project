0f58eb5b3073141f5018e999420e9b15e56cab8b
Loyalfae <wafa.faquir@student.hamk.fi>
2022-02-22 22:29:04 +0200

    commit

--- project 5/Header.php
+++ project 5/Header.php
@@ -1,9 +1,9 @@
 <header>
     <a class="logo" href=""><img class="logo-image" src="images/AMW.png" alt="Logo"/></a>
     <nav class="navigation">
-        <a href="index.html">Home</a>
-        <a href="portfolio.html">Portfolio</a>
-        <a href="contact.html">Contact</a>
+        <a href="index.php">Home</a>
+        <a href="portfolio.php">Portfolio</a>
+        <a href="contact.php">Contact</a>
         <a href="signup.php">Sign up</a>
         <a href="login.php">Login</a>
     </nav>
