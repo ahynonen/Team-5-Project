68eeb66eaff3d06794bb6210307d5c9725411e17
maj3stiic <90041668+maj3stiic@users.noreply.github.com>
2022-03-07 12:44:53 +0200

    main

--- signup/submit.php
+++ signup/submit.php
@@ -1,3 +1,4 @@
+
 <?php 
 include 'db.php';
 include_once 'header.php';
@@ -8,23 +9,14 @@ $lname= $_POST['lname'];
 $email= $_POST['email'];
 $pwd= $_POST['pwd'];
 
-<<<<<<< HEAD:signup/submit.php
 //insert Data into mysql database
-=======
-if (empty($_POST['email'])) {
-    echo "<h1>Please input your Email Address</h1>";
-} 
-if (empty($_POST['pwd'])) {
-    echo "<h1>Please input your Password</h1>";
-}
-    else {
->>>>>>> d195e8733538eabdb684222b13185458c75a51be:signup/create.php
 $sql="insert into users (fname, lname, email, pwd)
 values('$fname', '$lname', '$email', '$pwd')";
 
 if($conn->query($sql) === TRUE) {
-    echo "Account created Successfully. Please ";
+   
     echo "<a href='login.php?' class='top'>Login </a>";
+    echo "<p class=\"try\"> Account created Successfully. Please:</p>";
   
 }
 else
